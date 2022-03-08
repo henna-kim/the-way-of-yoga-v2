83a4a34b8ec945a42b6c7c72223f2f512b73f710
Madniel <48990829+Madniel@users.noreply.github.com>
2022-03-08 10:28:34 +0100

    Improve code

--- scripts/Pose.py
+++ scripts/Pose.py
@@ -6,18 +6,19 @@ from stats import *
 from PIL import ImageFont, ImageDraw, Image
 import pyttsx3
 
+
 class PoseDetector:
     mpDraw = mp.solutions.drawing_utils
     mpPose = mp.solutions.pose
     pose = mpPose.Pose()
     arrows = []
-    first_time=True
+    first_time = True
     speech = {
         '↓': 'down',
         '↑': 'up',
         '←': 'left',
         '→': 'right',
-        'OK':''
+        'OK': ''
     }
 
     def __int__(self, side, mode=False, upBody=False, smooth=True,
@@ -29,13 +30,12 @@ class PoseDetector:
         self.trackCon = trackCon
         self.side = side
 
-
-
     def findPose(self, img, draw=True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.pose.process(imgRGB)
         if self.results.pose_landmarks and draw:
-            self.mpDraw.draw_landmarks(img, self.results.pose_landmarks, self.mpPose.POSE_CONNECTIONS, landmark_drawing_spec=None)
+            self.mpDraw.draw_landmarks(img, self.results.pose_landmarks, self.mpPose.POSE_CONNECTIONS,
+                                       landmark_drawing_spec=None)
         return img
 
     def text(self, A, B, C, correct_angle_min, angle):
@@ -45,7 +45,7 @@ class PoseDetector:
         if ver > hor:
             if B[1] > A[1]:
                 # if angle < correct_angle_min:
-                    return '→'
+                return '→'
             else:
                 if angle < correct_angle_min:
                     return '→'
@@ -64,15 +64,14 @@ class PoseDetector:
                 else:
                     return '↓'
 
-
-    def findPosition(self, img, side, draw=True):
+    def findPosition(self, img, draw=True):
         pl_list = []
         if self.results.pose_landmarks:
             for id, lm in enumerate(self.results.pose_landmarks.landmark):
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
                 pl_list.append([id, cx, cy])
-                if draw and id in PRINT_PARTS[side]:
+                if draw and id in PRINT_PARTS:
                     cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
 
         return pl_list
@@ -97,31 +96,32 @@ class PoseDetector:
             angle_text = 'OK'
         else:
             angle_text = self.text(positions[A], positions[B], positions[C], correct_angle_min, angle)
+
         textes.append(angle_text)
         color = GREEN if correct_angle_min < angle < correct_angle_max else RED
         place = (b[0], b[1] + 10) if pose else (b[0], b[1] - 10)
 
-        fontpath = "./simsun.ttc"  # the font that has this "º" symbol
-        font = ImageFont.truetype(fontpath, 32)
+        font = ImageFont.truetype("./simsun.ttc", 32)
         img_pil = Image.fromarray(img)
         draw = ImageDraw.Draw(img_pil)
         draw.text(place, angle_text, font=font, fill=color)
 
-        # cv2.putText(img, angle_text, place, cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
         img = np.array(img_pil)
         return img, textes
 
-    def runDetector(self, path, side, position, live=False, scale_percent=30):
+    def runDetector(self, path, position, live=False, scale_percent=30):
         video = 0 if live else path
         cap = cv2.VideoCapture(video)
         start = time.time()
+
         while True:
             success, image = cap.read()
-            if not success:
-                break
+            if not success: break
+
             elapsed = time.time() - start
-            time_to_wait = elapsed
-            if time_to_wait > 3:
+            checking = elapsed
+
+            if checking > 3:
                 if not all(x is None for x in self.arrows):
                     self.run_voice(self.arrows)
                 start = time.time()
@@ -135,17 +135,16 @@ class PoseDetector:
             img = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
 
             img = self.findPose(img)
-            positions = self.findPosition(img, side)
+            positions = self.findPosition(img)
 
             if len(positions) == 0:
                 print('No pose landmarks found')
                 break
-            temps=[]
 
+            temps = []
             for angles in position:
                 img, temp = self.calculate_angle(img, positions, *angles)
                 temps.append(temp)
-
 
             if self.first_time:
                 self.arrows = temps
@@ -159,27 +158,24 @@ class PoseDetector:
             cv2.waitKey(1)
 
     def run_voice(self, lists):
-        print(info_parts['warrior'])
-        print(lists)
-        for j, i in enumerate(lists):
-            if i in [['↓'],['↑'],['←'],['→']]:
+        for i, move in enumerate(lists):
+            if move in [['↓'], ['↑'], ['←'], ['→']]:
                 synthesizer = pyttsx3.init()
-                say_so = self.speech[i[0]]
                 synthesizer.say('Move your')
                 synthesizer.runAndWait()
-                number = info_parts['warrior']
-                print(number)
-                print(j)
-                body_name = body_part[number[j]]
+                synthesizer.stop()
+                number = info_parts['warrior'][i]
+                body_name = body_part[number]
                 synthesizer.say(body_name)
                 synthesizer.runAndWait()
-                synthesizer.say(say_so)
+                synthesizer.stop()
+                synthesizer.say(self.speech[move[0]])
                 synthesizer.runAndWait()
                 synthesizer.stop()
 
+
 if __name__ == '__main__':
-    position = warrior3_l
-    path = '../videos/warrior3_l.mp4'
-    side = path[-5]
+    position = warrior3
+    path = '../videos/warrior3.mp4'
     while True:
-        PoseDetector().runDetector(path, side, position, scale_percent=50)
+        PoseDetector().runDetector(path, position, scale_percent=50)
